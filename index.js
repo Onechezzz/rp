a8382c84081fac21900c3382fa272943a3f04e63
Onechezzz <maximilian.afanasyev@gmail.com>
2019-04-22 22:44:25 +0300

    Initial MongoDB, database and connection

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.listen(4000, ()=>{
+mongoose.connect("mongodb://localhost/muggers-db");
+
+app.use("/api", require("./api"))
+
+app.listen(4000, ()=>{//Первое это порт, второе это коллбек функция при обращении к серверу
   console.log("server is listening");
 })
